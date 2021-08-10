8bec701a1f2a4d16b35fb151570b4ab93e7efeb7
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-08-10 15:22:10 +1000

    Created basic website

---
+++ app.js
@@ -0,0 +1,16 @@
+//Entry point for backend
+//Code adapted from WEBINFO 
+//Styling suggestions: 
+//end line with ; 
+//make comments about chunks of code 
+//indent
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+	res.send('<h1>Website</h1>');
+})
+
+app.listen(3000, ()=> {
+	console.log('The website app is listening on port 3000!');
+})
