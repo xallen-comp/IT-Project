d205f83de8077088ee27d47c398d715566b9e22f
xallen-comp <80371868+xallen-comp@users.noreply.github.com>
2021-11-12 19:23:38 +1100

    COMP-175: test 13

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -41,8 +41,7 @@ const getReminders = async (req, res) => {
     let reminders = [];
     for(index in events){
         for(index2 in events[index].reminder){
-            console.log((new Date(events[index].start_time)) - (new Date(req.body.date)));
-            if((new Date(events[index].start_time)) - (new Date(req.body.date)) < 0){
+            if((new Date(events[index].start_time)) > 0){
                 let obj = {"title": events[index].title, "_id": events[index]._id, "reminder": events[index].reminder};
                 reminders.push(obj);
             }
